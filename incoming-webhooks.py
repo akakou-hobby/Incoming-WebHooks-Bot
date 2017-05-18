28f412f35b63f635781134e94d99c16c2541f07d
akakou <akakou571@gmail.com>
2017-05-18 17:41:35 +0900

    [add] finish simple incoming webhook bot class

---
+++ incoming-webhooks.py
@@ -0,0 +1,28 @@
+
+# coding:utf-8
+'''This program is class for incoming webhooks'''
+
+import requests
+import json
+
+
+class IncomingWebhooks:
+    """Incoming webhooks"""
+
+    def __init__(self, url, text, username=u'', icon_emoji=u'', link_names=0):
+        """Set Property"""
+        self.url = ''
+        self.data = json.dumps({
+            'text':         text,               # text
+            'username':     username,           # user name
+            'icon_emoji':   icon_emoji,         # profile emoji
+            'link_names':   link_names,         # mention
+        })
+
+    def send(self):
+        """Send to Slack"""
+        # send
+        requests.post(
+            self.url,
+            self.data
+        )
